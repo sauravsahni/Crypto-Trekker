a4fb38f26c1677f28614d8a9c6f175c4fc73bec9
sauravsahni <sauravsahni02@gmail.com>
2025-03-21 13:56:41 +0530

    configured vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Update base URL for GitHub Pages
 export default defineConfig({
+  base: "/CryptoTrekker/", 
   plugins: [react()],
-})
+});
