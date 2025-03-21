f570c7e9c14b7a73233d7933fdea391fd453e54e
sauravsahni <sauravsahni02@gmail.com>
2025-03-21 14:25:18 +0530

    fixed small bug

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom' // for routing
+import { HashRouter } from 'react-router-dom' // for routing
 import CoinContextProvider from './context/CoinContext.jsx'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CoinContextProvider>
         <App />
       </CoinContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
